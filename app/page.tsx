8175d47c2d2f8bb59e541c66b86f53dfea5f4fed
Sekler <seklertyoma@gmail.com>
2023-12-09 05:26:24 +0300

    feat: implement e2e tests for basic calcualtor input

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
 
           notifications.show({
             title: 'Success',
-            message: 'Code was succesfully compiled !',
+            message: 'Code was successfully compiled !',
             color: 'teal',
             icon: <IconCheck style={{ width: 18, height: 18 }} />,
             radius: 'md',
@@ -196,7 +196,7 @@ export default function Home() {
 
         notifications.show({
           title: 'Success',
-          message: 'Code was succesfully deleted !',
+          message: 'Code was successfully deleted !',
           color: 'teal',
           icon: <IconCheck style={{ width: 18, height: 18 }} />,
           radius: 'md',
---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,70 @@
+describe('Behavior of app is correct', () => {
+  
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('textarea').as('editor');
+    cy.get('span').contains('Send').parent().parent().as('send-btn');
+    cy.get('span').contains('Save Code').parent().parent().as('save-btn');
+    cy.get('span').contains('Load Code').parent().parent().as('load-btn');
+    cy.intercept("POST", '/submit').as('sendCode')
+  })
+
+  it('Textarea is empty and buttons disabled', () => {
+    cy.get('@editor').should('have.value', '');
+    cy.get('@send-btn').should('be.disabled');
+    cy.get('@save-btn').should('be.disabled');
+    cy.get('@load-btn').should('not.be.disabled');
+  })
+
+  it('Textarea input is correct and become enabled', () => {
+    const typeValue = '1+1';
+    cy.get('@editor').type(typeValue);
+
+    cy.get('@editor').should('have.value', typeValue);
+    cy.get('@send-btn').should('not.be.disabled');
+    cy.get('@save-btn').should('not.be.disabled');
+    cy.get('@load-btn').should('not.be.disabled');
+  })
+
+  it('Correct error notification is shown on not finished operation', () => {
+    const typeValue = '1+';
+    cy.get('@editor').type(typeValue).wait(1000);
+
+    cy.get('@send-btn').click();
+
+    cy.get('.mantine-Notifications-root').contains('Error');
+    cy.get('.mantine-Notifications-root').contains('After operation there should be expression');
+  })
+
+  it('Correct notification is shown on unexpected character', () => {
+    const typeValue = '1+-';
+    cy.get('@editor').type(typeValue).wait(1000);
+
+    cy.get('@send-btn').click();
+
+    cy.get('.mantine-Notifications-root').contains('Error');
+    cy.get('.mantine-Notifications-root').contains('Error at expression\'s cycle. Unexpected -');
+  })
+
+  it('Correct notification is shown on unexpected character', () => {
+    const typeValue = '(1+2*2';
+    cy.get('@editor').type(typeValue).wait(1000);
+
+    cy.get('@send-btn').click();
+
+    cy.get('.mantine-Notifications-root').contains('Error');
+    cy.get('.mantine-Notifications-root').contains('Opened parenthesis doesn\'t have closed corresponding one');
+  })
+
+  it('Correct notification is shown on success', () => {
+    const typeValue = '1+2';
+    cy.get('@editor').type(typeValue).wait(1000);
+
+    cy.get('@send-btn').click();
+
+    cy.get('.mantine-Notifications-root').contains('Success');
+    cy.get('.mantine-Notifications-root').contains('Code was successfully compiled !');
+
+    cy.get('.mantine-Alert-root .mantine-Text-root').contains('3');
+  })
+})
